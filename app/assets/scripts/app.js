87058bf62f9fcea4295090e27a403b1bec7f5884
Andrei K <andrei@12ak.com>
2022-03-23 21:32:49 -0400

    Just some practice with an object

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,19 @@
 import "../styles/styles.css";
 
+// Enables hot module replacement
 if (module.hot) {
 	module.hot.accept();
 }
+
+// Just some practice with an object
+var john = {
+	name: "John Doe",
+	faveColor: "blue",
+
+	// When a function belongs to a function, it's referred to as a method
+	greet: function() {
+		console.log("Hi, my name is " + john.name + " and my favorite color is " +  john.faveColor);
+	}
+}
+
+john.greet();
