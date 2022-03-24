3246dac5b832729588d39487f407e23ac4b77617
Andrei K <andrei@12ak.com>
2022-03-23 21:49:40 -0400

    Practice with a constructor function

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -15,5 +15,21 @@ var john = {
 		console.log("Hi, my name is " + john.name + " and my favorite color is " +  john.faveColor);
 	}
 }
-
 john.greet();
+
+
+// This is a more efficient approach when we want to create multiple objects that are very similar to each other
+// Example of a constructor function
+function Person(name, faveColor) {
+	this.name = name;
+	this.faveColor = faveColor;
+	this.greet = function() {
+		console.log("Hi, my name is " + this.name + " and my favorite color is " +  this.faveColor);
+	}
+}
+
+var bob = new Person("Bob", "blue");
+bob.greet();
+
+var jane = new Person("Jane", "green");
+jane.greet();
